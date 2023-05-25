04031ee51ebaa4488803b5faff810bb9e8cfabb1
zhurakovska <n.sinawka@gmail.com>
2023-05-25 16:04:31 +0300

    add folder-img

---
+++ src/js/review.js
@@ -0,0 +1,12 @@
+(() => {
+    const refs = {
+      openReviewBtn: document.querySelector(“[data-review-open]“),
+      closeReviewBtn: document.querySelector(“[data-review-close]“),
+      review: document.querySelector(“[data-review]“),
+    };
+    refs.openReviewBtn.addEventListener(“click”, toggleReview);
+    refs.closeReviewBtn.addEventListener(“click”, toggleReview);
+    function toggleReview() {
+      refs.review.classList.toggle(“is-hidden”);
+    }
+  })();
